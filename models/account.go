ae79e594400926f22a22c117c4f2745377f77691
aochuka <ochukaamon@gmail.com>
2026-03-30 14:59:49 +0300

    feat: add user and account models

---
+++ models/account.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type AccountType string
+
+const (
+	AccountTypeMain     AccountType = "main"
+	AccountTypeLocked   AccountType = "locked"
+	AccountTypeFlexible AccountType = "flexible"
+	AccountTypeGroup    AccountType = "group"
+)
+
+type Account struct {
+	ID        int         `json:"id"`
+	UserID    int         `json:"user_id"`
+	Type      AccountType `json:"type"`
+	Balance   float64     `json:"balance"`
+	CreatedAt time.Time   `json:"created_at"`
+	UpdatedAt time.Time   `json:"updated_at"`
+}
---
+++ models/user.go
@@ -0,0 +1,27 @@
+package models
+
+import "time"
+
+type (
+	SavingType string
+	DailyLimit int
+)
+
+const (
+	SavingTypeGroup    SavingType = "group"
+	SavingTypePersonal SavingType = "personal"
+	DailyLimit5        DailyLimit = 5
+	DailyLimit10       DailyLimit = 10
+)
+
+type User struct {
+	ID         int        `json:"id"`
+	Name       string     `json:"name"`
+	Email      string     `json:"email"`
+	Phone      string     `json:"phone"`
+	Password   string     `json:"-"`
+	SavingType SavingType `json:"saving_type"`
+	DailyLimit DailyLimit `json:"daily_limit"`
+	CreatedAt  time.Time  `json:"created_at"`
+	UpdatedAt  time.Time  `json:"updated_at"`
+}
