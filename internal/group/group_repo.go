7be93f190187896f3288cf3fa42f9bfd5a9c04f5
Amonoff <ochukaamon@gmail.com>
2026-04-06 00:47:00 +0300

    feat: add Group model and CreateGroup, AddGroupMember to group repository

--- internal/group/group_repo.go
+++ internal/group/group_repo.go
@@ -12,6 +12,37 @@ type GroupRepository struct {
 func NewGroupRepository(db *sql.DB) *GroupRepository {
 	return &GroupRepository{DB: db}
 }
+func (r *GroupRepository) CreateGroup(name string, targetAmount float64, createdBy int) (*models.Group, error) {
+	query := `
+		INSERT INTO groups (name, target_amount, created_by)
+		VALUES ($1, $2, $3)
+		RETURNING id, name, target_amount, current_amount, created_by, created_at, updated_at
+	`
+	group := &models.Group{}
+	err := r.DB.QueryRow(query, name, targetAmount, createdBy).Scan(
+		&group.ID,
+		&group.Name,
+		&group.TargetAmount,
+		&group.CurrentAmount,
+		&group.CreatedBy,
+		&group.CreatedAt,
+		&group.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return group, nil
+}
+
+func (r *GroupRepository) AddGroupMember(groupID, userID int) error {
+	query := `
+		INSERT INTO group_members (group_id, user_id)
+		VALUES ($1, $2)
+	`
+	_, err := r.DB.Exec(query, groupID, userID)
+	return err
+}
+
 func (r *GroupRepository) GetGroupProgress(groupID int) (*models.GroupProgress, error) {
 	query := `
 		SELECT id,name,target_amount - current_amount AS remaining_amount
--- models/group.go
+++ models/group.go
@@ -1,5 +1,17 @@
 package models
 
+import "time"
+
+type Group struct {
+	ID            int       `json:"id"`
+	Name          string    `json:"name"`
+	TargetAmount  float64   `json:"target_amount"`
+	CurrentAmount float64   `json:"current_amount"`
+	CreatedBy     int       `json:"created_by"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
+
 type GroupProgress struct {
 	GroupID          int     `json:"group_id"`
 	GroupName        string  `json:"group_name"`
